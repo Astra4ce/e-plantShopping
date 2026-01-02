e9267690eedbb43f58d0073e835a9990b0ae58d2
Astra4ce <astra4ce@gmail.com>
2026-01-02 13:37:34 -0500

    added optional features

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -268,6 +268,12 @@ function ProductList({ onHomeClick }) {
     const handleRemoveItem = (plant) => {
         dispatch(removeItem(plant))
     }
+
+    const computeCartQuantity = () => {
+        console.log("cart", cart)
+        const sum = cart.items.reduce((total, item) => total + item.quantity, 0)
+        return sum
+    }
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -285,7 +291,10 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                    <text x="53%" y="56%" text-anchor="middle" font-family="Arial" font-size="70" fill="white">
+                    {computeCartQuantity()}
+                    </text></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -295,7 +304,9 @@ function ProductList({ onHomeClick }) {
                             <div key={index}>
                                 <h1>{category.category}</h1>
                                 <div className="product-list"> {
-                                    category.plants.map((plant, plantIndex) => (
+                                    category.plants.map((plant, plantIndex) => {
+                                        const inCart = cart.items.some(item => item.name === plant.name);
+                                        return(
                                         <div className="product-card" key={plantIndex}>
                                             <img 
                                                 className="product-image"
@@ -305,12 +316,12 @@ function ProductList({ onHomeClick }) {
                                             <div className="product-description">{plant.description}</div>
                                             <div className="product-cost">{plant.cost}</div>
                                             <button 
-                                                className="product-button"
+                                                className={inCart ? "product-button added-to-cart" : "product-button"} disabled={inCart}
                                                 onClick={() => handleAddToCart(plant)}>
-                                                Add to Cart
+                                                {inCart ? "Added" : "Add to Cart" }
                                             </button>
                                         </div>   
-                                    ))
+                                    )})
                                 } </div>
                             </div>
                                
